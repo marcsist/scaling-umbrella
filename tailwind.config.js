4aebb867b688ec1a5513a48e5c4067ee3cb84801
Marcs <43764595+marcsist@users.noreply.github.com>
2021-07-31 17:07:22 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  mode: 'jit',
+  darkMode: false,
+  theme: {
+    container: {
+      center: true,
+      padding: '1.5rem',
+    },
+    debugScreens: {
+      position: ['bottom', 'right'],
+    },
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('tailwindcss-debug-screens'),
+  ],
+  purge: ['./src/**/*.{js,md,njk,svg}'],
+}
